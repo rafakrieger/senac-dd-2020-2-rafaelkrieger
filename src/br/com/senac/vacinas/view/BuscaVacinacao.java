a7f7394220ae4f316f3e5eaae88e5db2cc4d9ed2
Rafael Bruno Krieger <rafabruno@gmail.com>
2020-12-02 00:32:17 -0300

    Update BuscaVacinacao.java

--- src/br/com/senac/vacinas/view/BuscaVacinacao.java
+++ src/br/com/senac/vacinas/view/BuscaVacinacao.java
@@ -1,131 +1,94 @@
 package br.com.senac.vacinas.view;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+
 
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.text.MaskFormatter;
-
-import br.com.senac.vacinas.controller.PessoaController;
-import br.com.senac.vacinas.controller.VacinaController;
 import br.com.senac.vacinas.controller.VacinacaoController;
-import br.com.senac.vacinas.model.dao.PesquisadorDAO;
-import br.com.senac.vacinas.model.dao.PessoaDAO;
-import br.com.senac.vacinas.model.dao.VacinaDAO;
-import br.com.senac.vacinas.model.seletores.SeletorVacina;
 import br.com.senac.vacinas.model.seletores.SeletorVacinacao;
-import br.com.senac.vacinas.model.vo.PesquisadorVO;
-import br.com.senac.vacinas.model.vo.PessoaVO;
-import br.com.senac.vacinas.model.vo.VacinaVO;
 import br.com.senac.vacinas.model.vo.VacinacaoVO;
-
 import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-
 import java.awt.Font;
 import java.util.List;
-
 import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFormattedTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.text.ParseException;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
 import java.awt.event.ActionEvent;
 import javax.swing.JTable;
 import javax.swing.JSeparator;
 import javax.swing.SwingConstants;
-import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 
 public class BuscaVacinacao extends JPanel {
-	
-	private JComboBox comboBoxVacina;
 	private JComboBox comboBoxAvaliacao;
-	private JComboBox comboBoxPessoa;
 	DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/uuuu");
 	private JTable tableResultado;
-	private static final String[] AVALIACAO = {"1 - Péssima", "2 - Ruim", "3 - Regular", "4 - Boa", "5 - Ótima"};
+	private static final String[] AVALIACAO = { "1 - Péssima", "2 - Ruim", "3 - Regular", "4 - Boa", "5 - Ótima" };
 	private List<VacinacaoVO> vacinacoesConsultadas;
+	private JFormattedTextField formattedTextFieldDataInicio;
+	private JFormattedTextField formattedTextFieldDataFim;
 
 	/**
 	 * Create the panel.
-	 */	
+	 */
 	public BuscaVacinacao() {
-		
+
 		setBounds(0, 60, 450, 500);
 		this.setBackground(new Color(32, 178, 170));
 		this.setBorder(null);
 		this.setLayout(null);
-		
-		JLabel lblVacina = new JLabel("VACINA");
-		lblVacina.setForeground(Color.DARK_GRAY);
-		lblVacina.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		lblVacina.setBounds(10, 65, 134, 25);
-		this.add(lblVacina);
-		
-		VacinaDAO dao = new VacinaDAO();
-		List<VacinaVO> vacinas = dao.pesquisarTodos();		
-		comboBoxVacina = new JComboBox(vacinas.toArray());
-		comboBoxVacina.setSelectedIndex(-1);
-		comboBoxVacina.setFont(new Font("Segoe UI", Font.PLAIN, 14));
-		comboBoxVacina.setBounds(10, 90, 210, 30);
-		this.add(comboBoxVacina);
-		
-		JLabel lblPessoa = new JLabel("PESSOA");
+
+		JLabel lblPessoa = new JLabel("AVALIAÇÃO");
 		lblPessoa.setForeground(Color.DARK_GRAY);
 		lblPessoa.setFont(new Font("Segoe UI", Font.BOLD, 14));
 		lblPessoa.setBounds(10, 132, 179, 25);
-		this.add(lblPessoa);	
-		
+		this.add(lblPessoa);
+
 		comboBoxAvaliacao = new JComboBox(AVALIACAO);
 		comboBoxAvaliacao.setSelectedIndex(-1);
 		comboBoxAvaliacao.setFont(new Font("Segoe UI", Font.PLAIN, 14));
-		comboBoxAvaliacao.setBounds(230, 90, 210, 30);
+		comboBoxAvaliacao.setBounds(10, 156, 430, 30);
 		this.add(comboBoxAvaliacao);		
-			
-		PessoaDAO daoPessoa = new PessoaDAO();
-		List<PessoaVO> pessoas = daoPessoa.pesquisarTodos();
-		comboBoxPessoa = new JComboBox(pessoas.toArray());
-		comboBoxPessoa.setSelectedIndex(-1);
-		comboBoxPessoa.setFont(new Font("Segoe UI", Font.PLAIN, 14));
-		comboBoxPessoa.setBounds(10, 156, 430, 30);
-		add(comboBoxPessoa);
 
-		
-		JLabel lblAvaliacao = new JLabel("AVALIAÇÃO");
-		lblAvaliacao.setForeground(Color.DARK_GRAY);
-		lblAvaliacao.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		lblAvaliacao.setBounds(230, 66, 210, 25);
-		this.add(lblAvaliacao);
-
-		
 		JButton btnPesquisarVacinacao = new JButton("PESQUISAR");
 		btnPesquisarVacinacao.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				VacinacaoController controlador = new VacinacaoController();
-				SeletorVacinacao seletor = new SeletorVacinacao();
+				SeletorVacinacao seletor = new SeletorVacinacao();				
 				
-				seletor.setAvaliacao(comboBoxAvaliacao.getSelectedIndex() + 1);
-				seletor.setPessoa((PessoaVO)comboBoxPessoa.getSelectedItem());
-				seletor.setVacina((VacinaVO) comboBoxVacina.getSelectedItem());
-				
+				if (comboBoxAvaliacao.getSelectedIndex() > 0) {
+					seletor.setAvaliacao(comboBoxAvaliacao.getSelectedIndex() + 1);
+				}
+				seletor.setDataInicio(obterData(formattedTextFieldDataInicio.getText()));
+				seletor.setDataFim(obterData(formattedTextFieldDataFim.getText()));
+
 				List<VacinacaoVO> vacinacoes = controlador.listarVacinacoes(seletor);
-				atualizarTabelaVacinacao(vacinacoes);	
+				
+				atualizarTabelaVacinacao(vacinacoes);
+				
+				tableResultado.getColumnModel().getColumn(0).setPreferredWidth(20);
+				tableResultado.getColumnModel().getColumn(1).setPreferredWidth(40);
+				tableResultado.getColumnModel().getColumn(2).setPreferredWidth(200);
 			}
 		});
 		btnPesquisarVacinacao.setFont(new Font("Segoe UI", Font.BOLD, 11));
 		btnPesquisarVacinacao.setBounds(10, 212, 95, 35);
 		this.add(btnPesquisarVacinacao);
-		
+
 		JButton btnExcluir = new JButton("EXCLUIR");
 		btnExcluir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				VacinacaoVO vacinacao = new VacinacaoVO();
 				int selRow = tableResultado.getSelectedRow();
 
@@ -137,54 +100,58 @@ public class BuscaVacinacao extends JPanel {
 				String mensagem = vacinacaoController.excluir(vacinacao);
 				JOptionPane.showMessageDialog(null, mensagem);
 
-				comboBoxVacina.setSelectedIndex(-1);
-				comboBoxPessoa.setSelectedIndex(-1);
+				formattedTextFieldDataInicio.setText("");
+				formattedTextFieldDataFim.setText("");
 				comboBoxAvaliacao.setSelectedIndex(-1);
-				
+
 				limparTabela();
-				
+
 			}
 		});
 		btnExcluir.setFont(new Font("Segoe UI", Font.BOLD, 11));
 		btnExcluir.setBounds(233, 212, 95, 35);
 		this.add(btnExcluir);
-		
+
 		tableResultado = new JTable();
 		tableResultado.setBounds(10, 270, 430, 204);
+		tableResultado.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+		this.limparTabela();
 		this.add(tableResultado);
-		
+
 		JButton btnEditar = new JButton("EDITAR");
 		btnEditar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				VacinacaoVO vacinacao = new VacinacaoVO();
 				int selRow = tableResultado.getSelectedRow();
 				String id = tableResultado.getModel().getValueAt(selRow, 0).toString();
 
 				vacinacao.setIdVacinacao(Integer.parseInt(id));
-				vacinacao.setAvaliacao(comboBoxAvaliacao.getSelectedIndex() + 1);
-				vacinacao.setPessoa((PessoaVO)comboBoxPessoa.getSelectedItem());
-				vacinacao.setVacina((VacinaVO) comboBoxVacina.getSelectedItem());
 
-				VacinacaoController vacinacaoController = new VacinacaoController();
-				String mensagem = vacinacaoController.atualizarBusca(vacinacao);
-				JOptionPane.showMessageDialog(null, mensagem);
+				if (comboBoxAvaliacao.getSelectedIndex() >= 0) {
+					vacinacao.setAvaliacao(comboBoxAvaliacao.getSelectedIndex() + 1);					
+					}
 
-				comboBoxVacina.setSelectedIndex(-1);
-				comboBoxPessoa.setSelectedIndex(-1);
-				comboBoxAvaliacao.setSelectedIndex(-1);
-				limparTabela();
-			}
+					VacinacaoController vacinacaoController = new VacinacaoController();
+					String mensagem = vacinacaoController.atualizarBusca(vacinacao);
+					JOptionPane.showMessageDialog(null, mensagem);
+
+					formattedTextFieldDataInicio.setText("");
+					formattedTextFieldDataFim.setText("");
+					comboBoxAvaliacao.setSelectedIndex(-1);
+					limparTabela();
+				}			
 		});
 		btnEditar.setFont(new Font("Segoe UI", Font.BOLD, 11));
 		btnEditar.setBounds(123, 212, 95, 35);
 		this.add(btnEditar);
-		
+
 		JButton btnLimpar = new JButton("LIMPAR");
 		btnLimpar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				comboBoxVacina.setSelectedIndex(-1);
-				comboBoxPessoa.setSelectedIndex(-1);
+				formattedTextFieldDataInicio.setText("");
+				formattedTextFieldDataFim.setText("");
 				comboBoxAvaliacao.setSelectedIndex(-1);
 				limparTabela();
 			}
@@ -192,18 +159,18 @@ public class BuscaVacinacao extends JPanel {
 		btnLimpar.setFont(new Font("Segoe UI", Font.BOLD, 11));
 		btnLimpar.setBounds(345, 212, 95, 35);
 		add(btnLimpar);
-		
+
 		JSeparator separator = new JSeparator();
 		separator.setBounds(0, 42, 233, 12);
 		add(separator);
-		
+
 		JLabel lblConsultaDeAplicaes = new JLabel("Consulta de aplicações");
 		lblConsultaDeAplicaes.setHorizontalAlignment(SwingConstants.LEFT);
 		lblConsultaDeAplicaes.setForeground(Color.WHITE);
 		lblConsultaDeAplicaes.setFont(new Font("Segoe UI", Font.BOLD, 18));
 		lblConsultaDeAplicaes.setBounds(10, 11, 210, 31);
 		add(lblConsultaDeAplicaes);
-		
+
 		JButton btnNewButton = new JButton("Imprimir");
 		btnNewButton.setFont(new Font("Segoe UI", Font.BOLD, 11));
 		btnNewButton.addActionListener(new ActionListener() {
@@ -225,29 +192,90 @@ public class BuscaVacinacao extends JPanel {
 		btnNewButton.setBounds(340, 19, 89, 23);
 		add(btnNewButton);
 		
+		try {
+			MaskFormatter mascaraDataInicio = new MaskFormatter("##/##/####");		
+			formattedTextFieldDataInicio = new JFormattedTextField(mascaraDataInicio);
+			formattedTextFieldDataInicio.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+			formattedTextFieldDataInicio.setBounds(10, 90, 210, 30);
+			this.add(formattedTextFieldDataInicio);	
+		
+		} catch (ParseException e) {
+			JOptionPane.showMessageDialog(null, "Ocorreu um erro no sistema, entre em contato com o administrador.");
+			System.out.println("Causa da exceção: " + e.getMessage());
+		}
+		
+		try {
+			MaskFormatter mascaraDataFim = new MaskFormatter("##/##/####");		
+			formattedTextFieldDataFim = new JFormattedTextField(mascaraDataFim);
+			formattedTextFieldDataFim.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+			formattedTextFieldDataFim.setBounds(230, 90, 210, 30);
+			this.add(formattedTextFieldDataFim);
+		} catch (ParseException e) {
+			JOptionPane.showMessageDialog(null, "Ocorreu um erro no sistema, entre em contato com o administrador.");
+			System.out.println("Causa da exceção: " + e.getMessage());
+		}
+		
+		
+
+		
+		JLabel lblDataInicio = new JLabel("DATA INICIAL");
+		lblDataInicio.setForeground(Color.DARK_GRAY);
+		lblDataInicio.setFont(new Font("Segoe UI", Font.BOLD, 14));
+		lblDataInicio.setBounds(10, 65, 134, 25);
+		this.add(lblDataInicio);
+		
+		
+		JLabel lblDataFim = new JLabel("DATA FINAL");
+		lblDataFim.setForeground(Color.DARK_GRAY);
+		lblDataFim.setFont(new Font("Segoe UI", Font.BOLD, 14));
+		lblDataFim.setBounds(230, 66, 210, 25);
+		this.add(lblDataFim);
 
 	}
-	
+
+	private LocalDate obterData(String data) {
+		LocalDate dataFormatada = null;				
+		if (validarData(data)) {
+			dataFormatada = LocalDate.parse(data, dateFormat);
+		}	
+		return dataFormatada;
+	}
+
+	private boolean validarData(String data) {
+		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/uuuu").withResolverStyle(ResolverStyle.STRICT);				
+	    try {
+	    	LocalDate date = LocalDate.parse(data, dateFormatter);
+	        return true;
+	    } catch (DateTimeParseException e) {			    	
+	    	return false;			       
+	    } 
+	}
+
 	protected void atualizarTabelaVacinacao(List<VacinacaoVO> vacinacoes) {
-		
+
 		vacinacoesConsultadas = vacinacoes;
 
 		this.limparTabela();
 
 		DefaultTableModel modelo = (DefaultTableModel) tableResultado.getModel();
-		
-		for (VacinacaoVO vacinacao: vacinacoes) {
-			
-			String[] novaLinha = new String[] { vacinacao.getIdVacinacao()+ "", vacinacao.getVacina().getIdVacina()+ "", vacinacao.getPessoa().getNome(),
-					vacinacao.getAvaliacao() + "", };
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+		for (VacinacaoVO vacinacao : vacinacoes) {
+			String dataFormatada = vacinacao.getDataVacinacao().format(formatter);
+			String[] novaLinha = new String[] { vacinacao.getIdVacinacao() + "",
+					vacinacao.getVacina().getIdVacina() + "", vacinacao.getPessoa().getNome(),
+					vacinacao.getAvaliacao() + "", dataFormatada };
 			modelo.addRow(novaLinha);
+
 		}
 
 	}
 
 	private void limparTabela() {
-		tableResultado.setModel(new DefaultTableModel(new String[][] { { "#", "Vacina", "Pessoa", "Avaliação" }, },
-				new String[] { "#", "Vacina", "Pessoa", "Avaliação" }));
+		tableResultado
+				.setModel(new DefaultTableModel(new String[][] { { "#", "Vacina", "Pessoa", "Avaliação", "Data" }, },
+						new String[] { "#", "Vacina", "Pessoa", "Avaliação", "Data" }));
 
-}
+	}
 }
