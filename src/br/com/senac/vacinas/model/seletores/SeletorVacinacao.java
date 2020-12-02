cc2addd1a30c81e8ac7960dad9c56dd9a90b4363
Rafael Bruno Krieger <rafabruno@gmail.com>
2020-12-02 00:47:26 -0300

    Update SeletorVacinacao.java

--- src/br/com/senac/vacinas/model/seletores/SeletorVacinacao.java
+++ src/br/com/senac/vacinas/model/seletores/SeletorVacinacao.java
@@ -1,85 +1,102 @@
-package br.com.senac.vacinas.model.vo;
+package br.com.senac.vacinas.model.seletores;
 
 import java.time.LocalDate;
 
-public class PessoaVO {
+import br.com.senac.vacinas.model.vo.PessoaVO;
+import br.com.senac.vacinas.model.vo.VacinaVO;
+
+public class SeletorVacinacao {
 	
-	private int idPessoa;
-	private String nome;
-	private LocalDate dataNascimento;
-	private String sexo;
-	private String cpf;
-	private boolean voluntario;
+	private int idVacinacao;
+	private LocalDate dataInicio;
+	private LocalDate dataFim;
+	private int avaliacao;
 	
-	public PessoaVO() {
+	private int limite;
+	private int pagina;
+	
+	public SeletorVacinacao() {
 		super();
-	}
-
-	public PessoaVO(int idPessoa, String nome, LocalDate dataNascimento, String sexo, String cpf, boolean voluntario) {
-		super();
-		this.idPessoa = idPessoa;
-		this.nome = nome;
-		this.dataNascimento = dataNascimento;
-		this.sexo = sexo;
-		this.cpf = cpf;
-		this.voluntario = voluntario;
-	}
-
-	public int getIdPessoa() {
-		return idPessoa;
-	}
-
-	public void setIdPessoa(int idPessoa) {
-		this.idPessoa = idPessoa;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public LocalDate getDataNascimento() {
-		return dataNascimento;
-	}
-
-	public void setDataNascimento(LocalDate dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-
-	public String getSexo() {
-		return sexo;
-	}
-
-	public void setSexo(String sexo) {
-		this.sexo = sexo;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public boolean isVoluntario() {
-		return voluntario;
-	}
-
-	public void setVoluntario(boolean voluntario) {
-		this.voluntario = voluntario;
-	}
-
-	@Override
-	public String toString() {
-		return this.getNome();
+		this.limite = 0;
+		this.pagina = -1;
 	}
 	
+	public boolean temFiltro() {
+		
+		if(this.idVacinacao > 0) {
+			return true;
+		}
+		
+		if(this.avaliacao > 0) {
+			return true;
+		}
+		
+		if(this.dataInicio != null) {
+			return true;
+		}
+		
+		if(this.dataFim != null) {
+			return true;
+		}		
+		return false;
+	}	
+	
+	public boolean temPaginacao() {
+		return ((this.limite > 0) && (this.pagina > -1));
+	}
+	
+	public int getOffset() {
+		return (this.limite * (this.pagina - 1));
+	}
 
+	public int getIdVacinacao() {
+		return idVacinacao;
+	}
+
+	public void setIdVacinacao(int idVacinacao) {
+		this.idVacinacao = idVacinacao;
+	}
+
+	public int getAvaliacao() {
+		return avaliacao;
+	}
+
+	public void setAvaliacao(int avaliacao) {
+		this.avaliacao = avaliacao;
+	}	
+
+	public LocalDate getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(LocalDate dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public LocalDate getDataFim() {
+		return dataFim;
+	}
+
+	public void setDataFim(LocalDate dataFim) {
+		this.dataFim = dataFim;
+	}
+
+	public int getLimite() {
+		return limite;
+	}
+
+	public void setLimite(int limite) {
+		this.limite = limite;
+	}
+
+	public int getPagina() {
+		return pagina;
+	}
+
+	public void setPagina(int pagina) {
+		this.pagina = pagina;
+	}
 	
 	
+
 }
-
